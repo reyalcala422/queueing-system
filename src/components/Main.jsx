27979d099ae13b03aa5376ff7c730cbb8cb73237
reyalcala422 <rey.alcala422@gmail.com>
2025-01-17 15:44:17 +0800

    active button with key pressed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 
 import drmc from '../assets/pictures/image1.png'
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useQueueState } from 'rooks';
 export function Main() {
     const userButtonsRef = useRef([
@@ -13,7 +13,8 @@ export function Main() {
     const [currentNumber, setCurrentNumber] = React.useState(0);
     const [previousNumber, setPreviousNumber] = React.useState(null);
     const [upcomingNumber, setUpcomingNumber] = React.useState(null);
-
+    //
+    const [activeButton, setActiveButton] = useState(null);
     const resetQueue = () => {
         dequeue();
         setCurrentNumber(0);
@@ -72,26 +73,44 @@ export function Main() {
                 return;
         }
     };
+    //
+    const handleButtonClick = (buttonIndex) => {
+        setActiveButton(buttonIndex);
+    };
+
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.key >= '1' && event.key <= '4') {
+                handleButtonClick(parseInt(event.key));
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
 
     return (
         <div>
             {/* <div className="bg-[url('./src/assets/pictures/image1.png')]"> */}
-            <div onKeyDown={handleKeyPress} className="bg-[url('./src/assets/pictures/image1.png')]">
+            <div onKeyDown={handleKeyPress} className="bg-[url('./src/assets/pictures/image1.png')] h-screen">
 
                 {/* <h1>Queue System</h1> */}
                 <div className='flex flex-col items-center mt-4'>
-                    <span className='text-priority-size text-text-color font-priority-font underline underline-offset-8 xl:text-priority-size-xl xl:-mt-48'>{currentNumber}</span>
+                    <span className='text-priority-size text-text-color font-priority-font
+                    underline underline-offset-8 xl:text-priority-size-xl xl:-mt-48'>{currentNumber}</span>
                     <p className='text-2xl font-semibold stroke-2
                     stroke-sub-text xl:-mt-20 xl:text-4xl'>Current Number: </p>
                     <p className='text-2xl font-semibold stroke-2
                     stroke-sub-text xl:text-4xl'>Previous Number:
-                        <span className='text-[#6EACDA] text-5xl xl:text-9xlxl'>{previousNumber}</span></p>
-
+                        <span className='text-[#6EACDA] text-5xl xl:text-7xl'>{previousNumber}</span></p>
                     {/* <p>Upcoming Number: {upcomingNumber || 'Calculating...'}</p> */}
                 </div>
                 <div className='flex justify-center'>
                     <div className='grid grid-rows-2 xl:grid-cols-4'>
-                        <button onClick={() => addToQueue(1)}
+                        {/* <button onClick={() => addToQueue(1)}
                             className='active:text-black 
                         transition ease-in-out delay-150
                         hover:-translate-y-1 hover:scale-110 duration-0
@@ -99,9 +118,42 @@ export function Main() {
                         text-3xl fon-bold
                         bg-buttons px-2 py-3 ml-3 rounded-lg
                         hover:bg-cyan-800  text-white w-72 mt-2 font-bold
-                        hover:text-sub-text' autoFocus="true">Window 1</button>
+                        hover:text-sub-text' autoFocus="true">Window 1</button> */}
 
-                        <button onClick={() => addToQueue(2)}
+                        <button onClick={() => { addToQueue(1); handleButtonClick(1) }}
+                            className={`font-semibold text-white transition ease-in-out
+                            delay-150 hover:-translate-y-1 hover:scale-110 duration-0 text-3xl 
+                            bg-buttons px-2 py-3 w-72 ml-3
+                            fon-bold hover:text-sub-text rounded-lg
+                                ${activeButton === 1 ? 'bg-cyan-800 mt-2 underline underline-offset-8 hover:-translate-y-1 hover:scale-110 text-black '
+                                    : 'bg-blue-500'}`} autoFocus="true">Window 1</button>
+
+                        <button onClick={() => { addToQueue(2); handleButtonClick(2) }}
+                            className={`font-semibold text-white transition ease-in-out
+                            delay-150 hover:-translate-y-1 hover:scale-110 duration-0 text-3xl 
+                            bg-buttons px-2 py-3 w-72 ml-3
+                            fon-bold hover:text-sub-text rounded-lg
+                                ${activeButton === 2 ? 'bg-cyan-800 mt-2 underline underline-offset-8 hover:-translate-y-1 hover:scale-110 '
+                                    : 'bg-blue-500'}`} autoFocus="true">Window 2</button>
+
+                        <button onClick={() => { addToQueue(3); handleButtonClick(3) }}
+                            className={`font-semibold text-white transition ease-in-out
+                            delay-150 hover:-translate-y-1 hover:scale-110 duration-0 text-3xl 
+                            bg-buttons px-2 py-3 w-72 ml-3
+                            fon-bold hover:text-sub-text rounded-lg
+                                ${activeButton === 3 ? 'bg-cyan-800 mt-2 underline underline-offset-8 hover:-translate-y-1 hover:scale-110 '
+                                    : 'bg-blue-500'}`} autoFocus="true">Window 3</button>
+
+                        <button onClick={() => { addToQueue(4); handleButtonClick(4) }}
+                            className={`font-semibold text-white transition ease-in-out
+                            delay-150 hover:-translate-y-1 hover:scale-110 duration-0 text-3xl 
+                            bg-buttons px-2 py-3 w-72 ml-3
+                            fon-bold hover:text-sub-text rounded-lg
+                                ${activeButton === 4 ? 'bg-cyan-800 mt-2 underline underline-offset-8 hover:-translate-y-1 hover:scale-110 '
+                                    : 'bg-blue-500'}`} autoFocus="true">Window 4</button>
+
+
+                        {/* <button onClick={() => { addToQueue(2); handleButtonClick(2) }}
                             className='active:text-black 
                         transition ease-in-out delay-150
                         hover:-translate-y-1 hover:scale-110 duration-0
@@ -109,9 +161,9 @@ export function Main() {
                         text-3xl fon-bold 
                         bg-buttons px-2 py-3 ml-3 rounded-lg
                         hover:bg-cyan-800  text-white w-72 mt-2 font-bold
-                        hover:text-sub-text'>Window 2</button>
+                        hover:text-sub-text'>Window 2</button> */}
 
-                        <button onClick={() => addToQueue(3)}
+                        {/* <button onClick={() => addToQueue(3)}
                             className='active:text-black 
                         transition ease-in-out delay-150
                         hover:-translate-y-1 hover:scale-110 duration-0
@@ -121,7 +173,7 @@ export function Main() {
                         hover:bg-cyan-800  text-white w-72 mt-2 font-bold
                         hover:text-sub-text'>Window 3</button>
 
-                        <button onClick={() => addToQueue(0)}
+                        <button onClick={() => addToQueue(4)}
                             className='active:text-black 
                         transition ease-in-out delay-150
                         hover:-translate-y-1 hover:scale-110 duration-0
@@ -129,7 +181,7 @@ export function Main() {
                         text-3xl fon-bold 
                         bg-buttons px-2 py-3 ml-3 rounded-lg
                         hover:bg-cyan-800  text-white w-72 mt-2 font-bold
-                        hover:text-sub-text'>Window 4</button>
+                        hover:text-sub-text'>Window 4</button> */}
 
                         {/* <button onClick={resetQueue} className='px-3 py-2 bg-[#FF722C] text-3xl font-bold
                         text-white rounded-lg mt-3 active:text-red-500 hover:bg-[#FF4F2C] hover:text-black'>Minus Queue</button> */}
@@ -137,14 +189,14 @@ export function Main() {
                         {/* <button onClick={resetFunc} className='px-3 py-2 bg-[#FF722C] text-3xl font-bold
                         text-white rounded-lg mt-3 active:text-red-500 hover:bg-[#FF4F2C] hover:text-black '>Reset Queue</button> */}
                         <button className='px-3 py-2 bg-[#FF722C] text-3xl font-bold
-                        text-white rounded-lg mt-3 active:text-red-500 hover:bg-[#FF4F2C] hover:text-black hidden ' onClick={() => resetFunc(0)}>Reset!</button>
+                        text-white rounded-lg mt-3 active:text-red-500 hover:bg-[#FF4F2C] hover:text-black lg:hidden ' onClick={() => resetFunc(0)}>Reset!</button>
                     </div>
                 </div>
-                <div className='overflow-auto hover:overflow-y-auto h-36  flex flex-col justify-end -mt-5 items-center'>
+                <div className='overflow-auto hover:overflow-y-auto h-36 lg:h-60 flex flex-col justify-end -mt-1 items-center'>
                     {list.map((item, index) => (
                         <div key={index}>
                             {/* <p>Priority number: {index}: {item.name} (ID: {item.id})</p> */}
-                            <h2><span className='text-3xl'>Priority number: </span> <span className='text-7xl font-bold text-red-500 xl:text-7xl'>{index}</span>: <span className='text-5xl font-bold'>{item.name}</span> </h2>
+                            <h2><span className='text-4xl'>Priority number: </span> <span className='text-7xl font-bold text-red-500 xl:text-9xl'>{index}</span>: <span className='text-6xl font-bold'>{item.name}</span> </h2>
                         </div>
                     ))}
                 </div>
